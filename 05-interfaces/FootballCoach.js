cc5397383edb22716d18712273c8cda408250ca4
Rianto <rianto.rnt@gmail.com>
2021-12-17 11:42:22 +0800

    removing js file

--- 05-interfaces/FootballCoach.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FootballCoach = void 0;
-class FootballCoach {
-    getDailyWorkout() {
-        return "Hit 100 ball at the goal keeper.";
-    }
-}
-exports.FootballCoach = FootballCoach;
--- 05-interfaces/SkateboardCoach.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SkateboardCoach = void 0;
-class SkateboardCoach {
-    getDailyWorkout() {
-        return "Practice your Kickflip technique.";
-    }
-}
-exports.SkateboardCoach = SkateboardCoach;
